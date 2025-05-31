5cc54017c8247d7ad2c3872799463bbf974eb938
ShermanJackson <shermandavidjackson@gmail.com>
2025-05-31 20:40:23 +0530

    Update getLirricalIdeas.js

--- netlify/functions/getLirricalIdeas.js
+++ netlify/functions/getLirricalIdeas.js
@@ -1,13 +1,14 @@
-// File: netlify/functions/getLirricalIdeas.js (TEMPORARY TEST CODE)
+// File: netlify/functions/getLirricalIdeas.js (TEMPORARY MINIMAL TEST CODE)
+
 exports.handler = async function(event, context) {
-  console.log("--- LIRRICAL FUNCTION TEST: Invoked Successfully! ---");
-  console.log("Received event for test:", JSON.stringify(event));
-  
+  console.log("--- MINIMAL TEST FUNCTION: getLirricalIdeas invoked! ---");
+  console.log("Minimal Test - Event Received:", JSON.stringify(event.queryStringParameters || {note: "No query string parameters received"}));
+
   return {
-    statusCode: 418, // Using a distinct "I'm a teapot" error for this test
+    statusCode: 418, // Using a distinct "I'm a teapot" error code for this test
     body: JSON.stringify({ 
-      message: "Test function for LIRRICAL was called!",
-      dataReceived: event.queryStringParameters 
+      message: "This is the MINIMAL TEST function for LIRRICAL speaking!",
+      receivedParams: event.queryStringParameters || null
     }),
     headers: { "Content-Type": "application/json" },
   };
